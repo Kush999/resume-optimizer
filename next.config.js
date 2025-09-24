85db5dc4d3b9a8139259ef04c145b6efc8614b4c
acer <acer@Kushs-MacBook-Air.local>
2025-09-24 13:19:13 -0400

    Fixed build errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // appDir is now stable in Next.js 15, no experimental flag needed
 }
 
 module.exports = nextConfig
